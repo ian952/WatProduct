1e4fc3ec47c7f79dc0efa2efa4cf4842de8eb868
Will-Lo <will-i-am_95@hotmail.com>
2016-01-21 10:18:34 -0500

    Alchemy API

---
+++ test.js
@@ -0,0 +1,18 @@
+'use strict';
+var watson = require('watson-developer-cloud');
+ 
+var alchemy_language = watson.alchemy_language({
+  api_key: '437cec71bb1624d205590209fa9b0161e7f4fff4'
+});
+ 
+var params = {
+  targets: 'apple|person',
+  url: 'http://www.engadget.com/2014/12/11/apple-22-on-glassdoors-2015-best-places-to-work-list/'
+};
+ 
+alchemy_language.sentiment(params, function (err, response) {
+  if (err)
+    console.log('error:', err);
+  else
+    console.log(JSON.stringify(response, null, 2));
+});
