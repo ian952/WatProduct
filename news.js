c40974f73ac6d3bce6137415b4b9bfcb956deed8
jeffreyfei <jeffreyfei@msn.com>
2016-01-21 16:44:35 -0500

    newAPI

--- news.js
+++ news.js
@@ -6,21 +6,24 @@ var alchemy_data_news = watson.alchemy_data_news({
   api_key: '437cec71bb1624d205590209fa9b0161e7f4fff4'
 });
 
+//insert keyword here
+var keyWord = '';
+
 var params = {
   start: 'now-1d',
   end: 'now',
   //set number of entries returned
   count: 10,
-  'q.enriched.url.enrichedTitle.relations.relation': '|action.verb.text=acquire,object.entities.entity.type=Company|',
-  return: 'enriched.url.title,enriched.url.url'
+  'q.enriched.url.enrichedTitle.keywords.keyword.text':keyWord,
+  return: 'enriched.url.url,enriched.url.title'
 };
 
 alchemy_data_news.getNews(params, function (err, news) {
   if (err)
     console.log('error:', err);
   else {
-	
-	//include parsing functions here
+	//Insert function here
+	console.log(JSON.stringify(news, null, 2));
 
   }
 });
