faae4d83a6d6fb6186ef13e95d4198aeb4aa4901
ian952 <ianhu952@hotmail.com>
2016-01-21 18:10:17 -0500

    Got sentiment to work

--- main.js
+++ main.js
@@ -1,30 +1,76 @@
+'use strict';
 
-	// const readline = require('readline');
+var keyword = 'apple';
+var links = [{
+  //can add more parameters from the alchemy language sentiment documentation
+  targets: keyword,
+  url: "http://www.engadget.com/2014/12/11/apple-22-on-glassdoors-2015-best-places-to-work-list/"
+}];
+var total_score = 0.0;
+var total_weight = 0;
+var NUM_KEYWORD = 1;
+var score;
+var watson = require('watson-developer-cloud');
+var params;
 
-	// const rl = readline.createInterface({
-	//   input: process.stdin,
-	//   output: process.stdout
-	// });
+var alchemy_data_news = watson.alchemy_data_news({
+  api_key: '437cec71bb1624d205590209fa9b0161e7f4fff4'
+});
+var alchemy_language = watson.alchemy_language({
+  api_key: '437cec71bb1624d205590209fa9b0161e7f4fff4'
+});
 
-	// rl.question('What do you think of Node.js? ', (answer) => {
-	//   rl.close();
-	// });
+var events = require('events');
+var EventEmitter = events.EventEmitter;
 
-	
+var flowController = new EventEmitter();
 
-	var text = '{'+
-    '"result": {' +
-       ' "docs": [' +
-            '{ "id": "MzQ0MDAxMDI5NnwxNDUyODI0MjUw",' +
-                '"source": {' +
-                    '"enriched": {' +
-                        '"url": {' +
-                            '"url": "http://www.cnet.com/uk/videos/apple-looks-to-bring-wireless-air-buds-to-the-iphone-7/"'+
-                '}}}}]}}';
+flowController.on('doWork', function (a) {
+  if (a >= links.length) {
+    flowController.emit('finished');
+    return;
+  }
 
-     var newsAnalyzed = JSON.parse(text);
+  alchemy_language.sentiment(links[a], function (err, response) {
+		  	
+	  if (err)
+	    console.log('error:', err);
+	  else {
+	  	for (var j = 0; j < NUM_KEYWORD; j++) {
+		  	total_weight += 1;
+		  	total_score += response.results[j].sentiment.score;
+		  	//console.log (total_score);
+		  }
 
-	//(getNews (answer));
-	for (var i = 0; i < 1;i++) {
-		console.log(newsAnalyzed.result.docs[i].source.enriched.url.url);
-	}
+	  }
+	  flowController.emit('doWork', a + 1);
+	});
+
+});
+
+flowController.on('finished', function () {
+  	score = total_score / total_weight;
+	console.log(score);
+});
+
+flowController.emit('doWork', 0);
+
+
+
+
+	// var text = '{'+
+ //    '"result": {' +
+ //       ' "docs": [' +
+ //            '{ "id": "MzQ0MDAxMDI5NnwxNDUyODI0MjUw",' +
+ //                '"source": {' +
+ //                    '"enriched": {' +
+ //                        '"url": {' +
+ //                            '"url": "http://www.cnet.com/uk/videos/apple-looks-to-bring-wireless-air-buds-to-the-iphone-7/"'+
+ //                '}}}}]}}';
+
+ //     var newsAnalyzed = JSON.parse(text);
+
+	// //(getNews (answer));
+	// for (var i = 0; i < 1;i++) {
+	// 	console.log(newsAnalyzed.result.docs[i].source.enriched.url.url);
+	// }
