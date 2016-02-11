00653428f0774251e0bf5fd45dca8e805c885a49
Will-Lo <will-i-am_95@hotmail.com>
2016-02-11 00:26:17 -0500

    Merge branch 'master' of https://github.com/ian952/Watson-Challenge

--- Test/app.js
+++ Test/app.js
@@ -1,4 +1,3 @@
-'use strict';
 /*
  * Module dependencies
  */
@@ -8,35 +7,6 @@ var express = require('express')
 
 
 var app = express()
-var keyword = 'Surface';
-var links = [];
-var total_score = 0;
-var total_weight = 0;
-var NUM_KEYWORD = 1;
-var NUM_LINKS = 5;
-var score = 0;
-var watson = require('watson-developer-cloud');
-var params = {
-  start: 'now-1d',
-  end: 'now',
-  //set number of entries returned
-  count: NUM_LINKS,
-  'q.enriched.url.enrichedTitle.keywords.keyword.text':keyword,
-  return: 'enriched.url.url,enriched.url.title'
-};
-
-var alchemy_data_news = watson.alchemy_data_news({
-  api_key: 'b87dde35a90f3c811699dd78c9a78ef86cb255a3'
-});
-var alchemy_language = watson.alchemy_language({
-  //api_key: '437cec71bb1624d205590209fa9b0161e7f4fff4'
-  api_key: 'b87dde35a90f3c811699dd78c9a78ef86cb255a3'
-});
-
-var events = require('events');
-var EventEmitter = events.EventEmitter;
-
-var flowController = new EventEmitter();
 
 function compile(str, path) {
   return stylus(str)
@@ -55,87 +25,9 @@ app.use(stylus.middleware(
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function (req, res) {
-  flowController.emit('news');
-  flowController.on ('render',function () {
-
-    var msg;
-    if (score > 0.5) {
-      msg = 'Overwhelmingly Positive';
-    } else if (score > 0.05 && score <= 0.5) {
-      msg = 'Positive';
-    } else if (score >= -0.05 && score <= 0.05) {
-      msg = 'Neutral';
-    } else if (score < -0.05 && score >= -0.5) {
-      msg = 'Negative';
-    } else if (score < -0.5){
-      msg = 'Overwhelmingly Negative';
-    } else {
-      msg = 'Error';
-    }
-
-    res.render('index',
-    { title : 'Home', product : keyword, scoreout : msg }
-    )
-  })
+  res.render('index',
+  { title : 'Home' }
+  )
 })
-
-
-
-flowController.on('news', function() {
-  alchemy_data_news.getNews(params, function (err, news) {
-    if (err)
-      console.log('error:', err);
-    else {
-    for (var i = 0; i < NUM_LINKS; i ++) {
-      //console.log(news.result.docs[i].source.enriched.url.url);
-      links.push ({targets: keyword, url: news.result.docs[i].source.enriched.url.url});
-    }
-    }
-
-    flowController.emit ('sentiment',0);
-  });
-});
-
-flowController.on('sentiment', function (a) {
-  if (a >= links.length) {
-    flowController.emit('finished');
-    return;
-  }
-
-  alchemy_language.sentiment(links[a], function (err, response) {
-    if (err){
-      //console.log('error:', err);
-    //console.log (links[a].url);
-    }else {
-      for (var j = 0; j < NUM_KEYWORD; j++) {
-
-          total_weight += 1;
-          if (!(response.results[j].sentiment.type == 'neutral')) {
-            total_score += parseFloat(response.results[j].sentiment.score);
-          }
-          //console.log (JSON.stringify(response, null, 2));
-
-        
-
-        //console.log (links[a].url);
-        //console.log (response.results[j].sentiment.score);
-
-        //console.log (total_weight);
-        //console.log (total_score);
-      }
-
-    }
-    flowController.emit('sentiment', a + 1);
-  });
-
-});
-
-flowController.on('finished', function () {
-    score = total_score / total_weight;
-    //console.log (total_score);
-    //console.log (total_weight);
-    console.log(score);
-    flowController.emit('render');
-});
 
 app.listen(3000)
